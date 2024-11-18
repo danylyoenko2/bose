4642c6a29e13ffd144a7fd3cb7f1ab344929caa9
danylyoenko2 <162820512+danylyoenko2@users.noreply.github.com>
2024-11-18 08:26:05 +0000

    Deploying to gh-pages from @ danylyoenko2/bose@25ceba10966cf49942120222d7cb562f5e463708 🚀

---
+++ index.ada1e261.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".nav__menu"),t=document.querySelector(".burger");t.addEventListener("click",(function(){e.classList.toggle("open"),t.classList.toggle("active"),document.querySelector("body").classList.toggle("no-scroll")}));
+//# sourceMappingURL=index.ada1e261.js.map
