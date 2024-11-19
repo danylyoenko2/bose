46a11e866d0696e351d8386d2f0fec1bc1cfecb1
danylyoenko <lockmaster19@gmail.com>
2024-11-19 21:28:30 +0200

    saved

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const menu = document.querySelector('.nav__menu');
+const menu = document.querySelector('.nav');
 const btn = document.querySelector('.burger');
 
 btn.addEventListener('click', function () {
   menu.classList.toggle('open');
   btn.classList.toggle('active');
 
+  document.querySelector('.header').classList.toggle('active');
   document.querySelector('body').classList.toggle('no-scroll');
 });
