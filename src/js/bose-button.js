0462bd513299381df3bb775219010a6a33e84869
danylyoenko <lockmaster19@gmail.com>
2024-12-10 22:00:09 +0200

    ended fixed

--- src/js/bose-button.js
+++ src/js/bose-button.js
@@ -1,17 +1,19 @@
-const controls = document.querySelector("[data-slider-nav]");
-const slider = document.querySelector("[data-container]");
+const controls = document.querySelector('[data-slider-nav]');
+const slider = document.querySelector('[data-container]');
 
-const handleSlider = (event) => {
+const handleSlider = event => {
   const target = event.target;
 
-  if (target.tagName === "BUTTON") {
+  if (target.tagName === 'BUTTON') {
     const index = target.dataset.slide;
-    slider.style.transform = `translateX(-${index}00%)`;
+    const translateX = `translateX(-${index}00%)`;
+    slider.style.transform = translateX;
 
-    document.querySelectorAll("[data-slide-nav]").forEach((btn) => {
-      btn.classList.remove("active");
+    document.querySelectorAll('[data-slide]').forEach(btn => {
+      btn.classList.remove('active-btn');
     });
-    target.classList.add("active");
+    target.classList.add('active-btn');
   }
 };
-controls.addEventListener("click", handleSlider);
+
+controls.addEventListener('click', handleSlider);
